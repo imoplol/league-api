51c96e63eff4967631ddecb8784f52b07544ad29
Siyuan Gao <siyuangao@gmail.com>
2015-09-26 21:38:36 -0700

    Initial Project Setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * index.js
+ *
+ * @author  Siyuan Gao <siyuangao@gmail.com>
+ * @license MIT
+ */
+
+/*!
+ *  League of Legends API Driver for imop.lol
+ */
+
+class LeagueDriver {
+
+}
+
+export default LeagueDriver;
