ad51bcb52a7a04c7e4c735d3f78becd1d9911cb5
Rock Hu <rockia@mac.com>
2015-10-25 03:09:39 -0700

    Reorganize index test

--- test/index.spec.js
+++ test/index.spec.js
@@ -15,17 +15,7 @@ Chai.should();
 var expect         = Chai.expect;
 
 describe('Index', function(){
-    it('should return APIDriver Object', function(){
-
-      var options = {
-        api_key: '1234',
-        platform: 'production',
-        region: 'NA'
-      }
-      class APIDriver{};
-      var apidriverObj      = new APIDriver(Endpoints,API,options);
-      expect(apidriverObj).to.be.an.instanceof(APIDriver);
-    });
+    require('./index/core.spec.js');
 });
 
 describe('Util', function() {
---
+++ test/index/core.spec.js
@@ -0,0 +1,36 @@
+/**
+ * util/core.spec.js
+ *
+ * @author  Rock Hu <rockia@mac.com>
+ * @license MIT
+ */
+ import Endpoints    from '../../src/config/endpoints.json';
+ import API          from '../../src/config/api.json';
+
+
+var Sinon           = require('sinon');
+var Chai            = require('chai');
+var Path            = require('path');
+
+Chai.use(require('sinon-chai'));
+Chai.should();
+
+var expect         = Chai.expect;
+
+describe('index', function() {
+  var options = {
+    api_key: '1234',
+    platform: 'production',
+    region: 'NA'
+  };
+  class APIDriver{};
+  var apidriverObj      = new APIDriver(Endpoints,API,options);
+
+  it('should return APIDriver Object', function(){
+    expect(apidriverObj).to.be.an.instanceof(APIDriver);
+  });
+
+  it('should return correct tag name', function(){
+    expect(apidriverObj.TAG).to.equal('APIDriver:constructor');
+  });
+});
