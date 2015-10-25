a7fa6cb75d9a790f59e99a5ad3a675508c6b7698
Rock Hu <rockia@mac.com>
2015-10-25 02:44:37 -0700

    Test APIDriver

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,10 +5,28 @@
  * @license MIT
  */
 
+ import Endpoints    from '../src/config/endpoints.json';
+ import API          from '../src/config/api.json';
+
 var Chai           = require('chai');
 Chai.use(require('sinon-chai'));
 Chai.use(require('chai-as-promised'));
 Chai.should();
+var expect         = Chai.expect;
+
+describe('Index', function(){
+    it('should return APIDriver Object', function(){
+
+      var options = {
+        api_key: '1234',
+        platform: 'production',
+        region: 'NA'
+      }
+      class APIDriver{};
+      var apidriverObj      = new APIDriver(Endpoints,API,options);
+      expect(apidriverObj).to.be.an.instanceof(APIDriver);
+    });
+});
 
 describe('Util', function() {
     require('./util/core.spec.js');
--- test/util/core.spec.js
+++ test/util/core.spec.js
@@ -4,21 +4,26 @@
  * @author  Rock Hu <rockia@mac.com>
  * @license MIT
  */
+ import Endpoints    from '../../src/config/endpoints.json';
+ import API          from '../../src/config/api.json';
+
 
 var Sinon           = require('sinon');
 var Chai            = require('chai');
 var Path            = require('path');
-
+var urlBuilder      = require('../../src/util/url-builder.js');
 
 Chai.use(require('sinon-chai'));
-Chai.use(require('chai-as-promised'));
 Chai.should();
 
-Chai.use(require('chai-as-promised'));
 var expect         = Chai.expect;
 
 describe('utils', function() {
-    it('should give correct destination', function() {
-      
-    });
+  it('should build correct url', function() {
+    var region = 'NA';
+    var name = 'summoner';
+    var params = '';
+    var URLParams = '';
+    // expect(urlBuilder(Endpoints, API, region, name, params, URLParams)).to.equal([]);
+  });
 });
