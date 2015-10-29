a11ec8a9ab4c57f623a91f0f925b22656b5ef5ff
Rock Hu <rockia@mac.com>
2015-10-29 00:45:32 -0700

    Test urlParams

--- test/util/core.spec.js
+++ test/util/core.spec.js
@@ -19,12 +19,30 @@ Chai.should();
 var expect         = Chai.expect;
 
 describe('utils', function() {
-  it('should build correct url', function() {
-    var region = 'NA';
-    var name = 'summoner-info';
-    var params = {summonerName: 'Mamoritai'};
-    var URLParams = '';
-    var result = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Mamoritai";
-    expect(urlBuilder(Endpoints, API, region, name, params, URLParams)[0]).equal(result);
-  });
+    it('should build correct url', function() {
+        var region = 'NA';
+        var name = 'summoner-info';
+        var params = {summonerName: 'Mamoritai'};
+        var URLParams = '';
+        var result = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Mamoritai";
+        expect(urlBuilder(Endpoints, API, region, name, params, URLParams)[0]).equal(result);
+    });
+
+    it('should return correct url with 1 URLParam', function(){
+        var region = 'NA';
+        var name = 'summoner-info';
+        var params = {summonerName: 'Mamoritai'};
+        var URLParams = ["test"];
+        var result = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Mamoritai?test";
+        expect(urlBuilder(Endpoints, API, region, name, params, URLParams)[0]).equal(result);
+    });
+
+    it('should return correct url with 2 or more URLParams', function(){
+        var region = 'NA';
+        var name = 'summoner-info';
+        var params = {summonerName: 'Mamoritai'};
+        var URLParams = ["latest","clean"];
+        var result = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Mamoritai?latest&clean";
+        expect(urlBuilder(Endpoints, API, region, name, params, URLParams)[0]).equal(result);
+    });
 });
